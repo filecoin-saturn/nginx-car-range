5484e9fe80407793bf6e1c91150fd6b4bab04b4a
tchardin <tdotchardin@gmail.com>
2023-03-06 11:50:21 -0600

    chore: refactor into lib structure

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ fn main() {
     prost_build::Config::new()
         .bytes([".unixfs_pb.Data", ".merkledag_pb.PBNode.Data"])
         .compile_protos(&["src/unixfs.proto", "src/merkledag.proto"], &["src"])
-        .unwrap();
+        .expect("unable to generate unixfs protobufs");
 }
