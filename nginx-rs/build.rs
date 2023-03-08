989af0b57b555abe89ac2ee802e9a3a5362fa676
tchardin <tdotchardin@gmail.com>
2023-03-08 16:58:28 -0600

    try updating the build script

--- nginx-rs/build.rs
+++ nginx-rs/build.rs
@@ -1,34 +1,37 @@
+extern crate bindgen;
+
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Path to the nginx repo in the local file system
     let nginx_dir = env::var("NGINX_DIR").unwrap_or(String::from("../../nginx"));
 
-    let clang_args = [
-        format!("-I{}/objs", nginx_dir),
-        format!("-I{}/src/core", nginx_dir),
-        format!("-I{}/src/event", nginx_dir),
-        format!("-I{}/src/event/modules", nginx_dir),
-        format!("-I{}/src/os/unix", nginx_dir),
-        format!("-I{}/src/http", nginx_dir),
-        format!("-I{}/src/http/modules", nginx_dir),
-    ];
-
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
     let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
         .header("wrapper.h")
         .layout_tests(false)
-        .allowlist_type("ngx_.*")
-        .allowlist_function("ngx_.*")
-        .allowlist_var("NGX_.*|ngx_.*|nginx_.*")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .clang_args(clang_args)
+        .whitelist_type("ngx_.*")
+        .whitelist_function("ngx_.*")
+        .whitelist_var("NGX_.*|ngx_.*|nginx_.*")
+        .clang_arg(format!("-I{}/src/core", nginx_dir))
+        .clang_arg(format!("-I{}/src/event", nginx_dir))
+        .clang_arg(format!("-I{}/src/event/modules", nginx_dir))
+        .clang_arg(format!("-I{}/src/os/unix", nginx_dir))
+        .clang_arg(format!("-I{}/objs", nginx_dir))
+        .clang_arg(format!("-I{}/src/http", nginx_dir))
+        .clang_arg(format!("-I{}/src/http/modules", nginx_dir))
+        // Finish the builder and generate the bindings.
         .generate()
-        .expect("unable to generate nginx bindings");
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
 
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("unable to write nginx bindings.");
+        .expect("Couldn't write bindings!");
 }
