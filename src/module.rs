4d591ec7154a5d7cdb88f3a29d95fcff547579a9
tchardin <tdotchardin@gmail.com>
2023-03-09 15:59:13 -0600

    extra request args

--- src/module.rs
+++ src/module.rs
@@ -1,4 +1,5 @@
 use crate::bindings::*;
+use crate::request::*;
 use std::os::raw::{c_char, c_void};
 use std::ptr;
 
@@ -121,58 +122,6 @@ unsafe extern "C" fn ngx_car_range(
     ptr::null_mut()
 }
 
-impl ngx_str_t {
-    /// Yields a `&str` slice if the [`NgxStr`] contains valid UTF-8.
-    fn to_str(&self) -> Result<&str, std::str::Utf8Error> {
-        // SAFETY: The caller has provided a valid `ngx_str_t` with a `data` pointer that points
-        // to range of bytes of at least `len` bytes, whose content remains valid and doesn't
-        // change for the lifetime of the returned `NgxStr`.
-        let bytes = unsafe { std::slice::from_raw_parts(self.data, self.len) };
-        std::str::from_utf8(bytes)
-    }
-}
-
-#[repr(transparent)]
-pub struct Request(ngx_http_request_t);
-
-impl Request {
-    /// Create a [`Request`] from an [`ngx_http_request_t`].
-    ///
-    /// [`ngx_http_request_t`]: https://nginx.org/en/docs/dev/development_guide.html#http_request
-    unsafe fn from_ngx_http_request<'a>(r: *mut ngx_http_request_t) -> &'a mut Request {
-        // SAFETY: The caller has provided a valid non-null pointer to a valid `ngx_http_request_t`
-        // which shares the same representation as `Request`.
-        &mut *r.cast::<Request>()
-    }
-
-    fn connection(&self) -> *mut ngx_connection_t {
-        self.0.connection
-    }
-
-    fn range(&self) -> Option<&str> {
-        if self.0.headers_in.range.is_null() {
-            return None;
-        }
-        unsafe { (*self.0.headers_in.range).value.to_str().ok() }
-    }
-
-    fn set_status(&mut self, status: ngx_uint_t) {
-        self.0.headers_out.status = status;
-    }
-
-    fn set_content_length(&mut self, n: usize) {
-        self.0.headers_out.content_length_n = n as off_t;
-    }
-
-    fn set_content_type(&mut self, ct: ngx_str_t) {
-        self.0.headers_out.content_type = ct;
-    }
-
-    fn send_header(&mut self) -> ngx_int_t {
-        unsafe { ngx_http_send_header(&mut self.0) }
-    }
-}
-
 #[no_mangle]
 extern "C" fn ngx_car_range_handler(r: *mut ngx_http_request_t) -> ngx_int_t {
     let req = unsafe { &mut Request::from_ngx_http_request(r) };
@@ -183,9 +132,9 @@ extern "C" fn ngx_car_range_handler(r: *mut ngx_http_request_t) -> ngx_int_t {
     let range = req.range();
 
     let body = if let Some(range_val) = range {
-        format!("Detected range header {}", range_val)
+        format!("Detected args {}\n", range_val)
     } else {
-        "Not a range request".to_string()
+        "Not a range request\n".to_string()
     };
 
     req.set_status(NGX_HTTP_OK as ngx_uint_t);
