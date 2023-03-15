c6b878a2789b65c80ada6ec9c1ed9cee409fbfb1
tchardin <tdotchardin@gmail.com>
2023-03-14 18:15:39 -0600

    try different string impl

--- src/request.rs
+++ src/request.rs
@@ -13,6 +13,49 @@ impl ngx_str_t {
     }
 }
 
+pub struct NgxStr([u_char]);
+
+impl NgxStr {
+    /// Create an [`NgxStr`] from an [`ngx_str_t`].
+    ///
+    /// [`ngx_str_t`]: https://nginx.org/en/docs/dev/development_guide.html#string_overview
+    pub unsafe fn from_ngx_str<'a>(str: ngx_str_t) -> &'a NgxStr {
+        // SAFETY: The caller has provided a valid `ngx_str_t` with a `data` pointer that points
+        // to range of bytes of at least `len` bytes, whose content remains valid and doesn't
+        // change for the lifetime of the returned `NgxStr`.
+        std::slice::from_raw_parts(str.data, str.len).into()
+    }
+
+    /// Access the [`NgxStr`] as a byte slice.
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+
+    /// Yields a `&str` slice if the [`NgxStr`] contains valid UTF-8.
+    pub fn to_str(&self) -> Result<&str, std::str::Utf8Error> {
+        std::str::from_utf8(self.as_bytes())
+    }
+
+    /// Converts an [`NgxStr`] into a [`Cow<str>`], replacing invalid UTF-8 sequences.
+    ///
+    /// See [`String::from_utf8_lossy`].
+    pub fn to_string_lossy(&self) -> Cow<str> {
+        String::from_utf8_lossy(self.as_bytes())
+    }
+
+    /// Returns `true` if the [`NgxStr`] is empty, otherwise `false`.
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl From<&[u8]> for &NgxStr {
+    fn from(bytes: &[u8]) -> Self {
+        // SAFETY: An `NgxStr` is identical to a `[u8]` slice, given `u_char` is an alias for `u8`.
+        unsafe { &*(bytes as *const [u8] as *const NgxStr) }
+    }
+}
+
 fn parse_bound(s: &str) -> Option<Bound<u64>> {
     if s == "*" {
         return Some(Bound::Unbounded);
@@ -93,7 +136,9 @@ impl Request {
                 continue;
             }
 
-            if let Ok(k) = key.to_str() {
+            let keystr = unsafe { NgxStr::from_ngx_str(key) };
+
+            if let Ok(k) = keystr.to_str() {
                 //     if i == 1 {
                 //         return k.to_string();
                 //     }
