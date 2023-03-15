c66d298022c4fcc83d2671f65656db2e26d5bbd7
tchardin <tdotchardin@gmail.com>
2023-03-15 12:45:45 -0600

    Check if request accepts CAR files (#9)
    Co-authored-by: Will Scott <will.scott@protocol.ai>

--- src/request.rs
+++ src/request.rs
@@ -8,7 +8,7 @@ impl ngx_str_t {
         // SAFETY: The caller has provided a valid `ngx_str_t` with a `data` pointer that points
         // to range of bytes of at least `len` bytes, whose content remains valid and doesn't
         // change for the lifetime of the returned `NgxStr`.
-        let bytes = unsafe { std::slice::from_raw_parts(self.data, self.len) };
+        let bytes = unsafe { std::slice::from_raw_parts_mut(self.data, self.len) };
         std::str::from_utf8(bytes)
     }
 }
@@ -32,6 +32,7 @@ fn parse_range(args: &str) -> Option<(Bound<u64>, Bound<u64>)> {
     None
 }
 
+// Wrapper for the nginx http request to provide safer access and operations.
 #[repr(transparent)]
 pub struct Request(pub ngx_http_request_t);
 
@@ -52,6 +53,97 @@ impl Request {
     pub fn range(&self) -> Option<(Bound<u64>, Bound<u64>)> {
         let args = self.0.args.to_str().ok()?;
         parse_range(args)
+    }
+
+    pub fn accept_car(&self) -> bool {
+        // Headers is a ngx list which is a sequence of arrays:
+        // struct ngx_list_t {
+        //     last: *mut ngx_list_part_t,
+        //     part: ngx_list_part_t,
+        //     size: usize,
+        //     nalloc: ngx_uint_t,
+        //     pool: *mut ngx_pool_t,
+        // }
+        // The array parts looks like:
+        // struct ngx_list_part_t {
+        //     elts: *mut ::std::os::raw::c_void,
+        //     nelts: ngx_uint_t,
+        //     next: *mut ngx_list_part_t,
+        // }
+        let headers = self.0.headers_in.headers;
+
+        // we iterate over the array and then go to the next one in the list
+        let mut part = headers.part;
+        let mut i = 0;
+
+        // There should at least be a few headers but just to be safe...
+        if part.elts.is_null() {
+            return false;
+        }
+
+        // Each HTTP header in the array is shaped as:
+        // struct ngx_table_elt_s {
+        //     hash: ngx_uint_t,
+        //     key: ngx_str_t,
+        //     value: ngx_str_t,
+        //     lowcase_key: *mut u_char,
+        //     next: *mut ngx_table_elt_t,
+        // }
+        // Create a slice over the first array in the list
+        let mut arr: &[ngx_table_elt_t] =
+            unsafe { std::slice::from_raw_parts(part.elts as *const ngx_table_elt_t, part.nelts) };
+
+        loop {
+            // only iterate first array for now
+            if i >= arr.len() {
+                if part.next.is_null() {
+                    break;
+                }
+                part = unsafe { *part.next };
+                arr = unsafe {
+                    std::slice::from_raw_parts(part.elts as *const ngx_table_elt_t, part.nelts)
+                };
+                i = 0;
+            }
+
+            let table = arr[i];
+
+            // increment the index for the next iteration
+            i += 1;
+
+            // the key and values are nginx string objects
+            // struct ngx_str_t {
+            //      len: usize,
+            //      data: *mut u_char,
+            // }
+            // create a byte slice from the nginx string object
+            let bytes = unsafe { std::slice::from_raw_parts(table.key.data, table.key.len) };
+            if bytes.is_empty() {
+                continue;
+            }
+
+            // As per RFC5987, the character set and language encoding in HTTP headers
+            // must be UTF-8 characters so we can skip the expensive validation check.
+            let k = unsafe { std::str::from_utf8_unchecked(bytes) };
+
+            if !k.contains("Accept") {
+                continue;
+            }
+
+            let bytes = unsafe { std::slice::from_raw_parts(table.value.data, table.value.len) };
+            if bytes.is_empty() {
+                continue;
+            }
+
+            let v = unsafe { std::str::from_utf8_unchecked(bytes) };
+
+            // Check that the Accept header is in CAR format
+            if v == "application/vnd.ipld.car" {
+                return true;
+            }
+        }
+
+        false
     }
 
     pub fn set_status(&mut self, status: ngx_uint_t) {
