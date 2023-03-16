12d81eba2b10dc208a21552efc54fb64f1e3ecea
tchardin <tdotchardin@gmail.com>
2023-03-16 13:10:41 -0600

    decode unixfs blocks

--- src/car_reader.rs
+++ src/car_reader.rs
@@ -5,9 +5,9 @@ use crate::request::Request;
 use crate::varint::VarInt;
 use cid::Cid;
 use core2::io::Cursor;
+use prost::Message;
 use serde::{Deserialize, Serialize};
-// use prost::Message;
-// use std::ops::{Bound, RangeBounds};
+use std::ops::RangeBounds;
 
 mod unixfs_pb {
     include!(concat!(env!("OUT_DIR"), "/unixfs_pb.rs"));
@@ -39,8 +39,11 @@ pub enum DataType {
 }
 
 pub fn read_car(req: &mut Request, input: *mut ngx_chain_t) -> Option<CarHeader> {
+    let range = req.range()?;
     let mut header: Option<CarHeader> = None;
     let mut cl = input;
+
+    let mut current = 0;
 
     while !cl.is_null() {
         let buf = unsafe { MemoryBuffer::from_ngx_buf((*cl).buf) };
@@ -52,14 +55,44 @@ pub fn read_car(req: &mut Request, input: *mut ngx_chain_t) -> Option<CarHeader>
             if let Some((size, read)) = usize::decode_var(&bytes[pos..]) {
                 let bound = size + read;
 
+                // The first frame is the CAR header
                 if header.is_none() {
                     header = serde_ipld_dagcbor::from_slice(&bytes[read..bound]).ok();
+                    continue;
                 }
 
+                // else it's just a block
                 let mut cursor = Cursor::new(&bytes[read..bound]);
                 if let Ok(cid) = Cid::read_bytes(&mut cursor) {
-                    // let blk = &bytes[cursor.position() as usize..bound];
+                    let data = &bytes[cursor.position() as usize..bound];
+
                     ngx_log_debug_http!(req, "car_range decoded block {:?}", cid);
+
+                    match cid.codec() {
+                        0x70 => {
+                            let outer = dag_pb::PbNode::decode(data).ok()?;
+                            let inner_data = outer.data.as_ref().cloned()?;
+                            let inner = unixfs_pb::Data::decode(inner_data).ok()?;
+
+                            // let dt: DataType = inner.r#type.try_into().ok()?;
+
+                            if outer.links.len() == 0 && inner.data.is_some() {
+                                let skip = !range.contains(&current);
+                                current += inner.data.as_ref().map(|d| d.len() as u64).unwrap();
+                                if skip {
+                                    continue;
+                                }
+                            }
+                        }
+                        0x55 => {
+                            let skip = !range.contains(&current);
+                            current += data.len() as u64;
+                            if skip {
+                                continue;
+                            }
+                        }
+                        _ => (),
+                    }
                 }
 
                 pos += bound;
