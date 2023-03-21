418ce7d5958e06e40bf7a82e458ecc77f14d4108
tchardin <tdotchardin@gmail.com>
2023-03-21 12:52:20 -0700

    try setting content type header

--- src/module.rs
+++ src/module.rs
@@ -131,7 +131,9 @@ extern "C" fn ngx_car_range_header_filter(r: *mut ngx_http_request_t) -> ngx_int
         bail!();
     }
 
+    req.set_content_length(0);
     req.set_content_length_missing();
+    req.set_content_type(ngx_string!("application/vnd.ipld.car; version=1"));
 
     bail!()
 }
