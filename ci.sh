3811ea5238400e024e09aaadab98bcd69bbaac3d
tchardin <tdotchardin@gmail.com>
2023-04-05 11:03:11 -0600

    download fixture from ipfs and request first block

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ set -x #echo on
 /etc/init.d/nginx stop
 /usr/local/nginx/sbin/nginx -c /etc/nginx/nginx.conf
 sleep 1
-curl -o partial.car -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car?bytes=0:1024
+curl -o partial.car -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car?bytes=0:262144
 ls -lh partial.car
 /usr/local/bin/car ls -v partial.car
 sleep 1
