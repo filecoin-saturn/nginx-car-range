436fc651a51bbb40dc5ee3013efaac9986ec3470
tchardin <tdotchardin@gmail.com>
2023-04-05 12:24:09 -0600

    print different test cases

--- ci.sh
+++ ci.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -x #echo on
 
+curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car
+ls -lh partial.car
+/usr/local/bin/car ls -v partial.car
+
 /etc/init.d/nginx stop
 /usr/local/nginx/sbin/nginx -c /etc/nginx/nginx.conf
 sleep 1
-curl -o partial.car -s -w "%{time_total}\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car?bytes=0:262144
+
+curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car?bytes=0:262144
 ls -lh partial.car
 /usr/local/bin/car ls -v partial.car
+
+curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car?bytes=262144:524288
+ls -lh partial.car
+/usr/local/bin/car ls -v partial.car
+
 sleep 1
 cat /var/log/nginx/error.log
