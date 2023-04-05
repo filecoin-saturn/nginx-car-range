232a99cb8475bc827dbd67ba949f4a270d1a343c
tchardin <tdotchardin@gmail.com>
2023-04-05 12:37:00 -0600

    enable module first

--- ci.sh
+++ ci.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x #echo on
 
-curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car
-ls -lh partial.car
-/usr/local/bin/car ls -v partial.car
-
 /etc/init.d/nginx stop
 /usr/local/nginx/sbin/nginx -c /etc/nginx/nginx.conf
 sleep 1
+
+curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car
+ls -lh partial.car
+/usr/local/bin/car ls -v partial.car
 
 curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car?bytes=0:262144
 ls -lh partial.car
