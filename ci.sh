09fca5e2e308a5754ab8fc3ea077b23cae430f61
tchardin <tdotchardin@gmail.com>
2023-04-06 13:11:31 -0600

    delete logs after first rangeless request

--- ci.sh
+++ ci.sh
@@ -8,6 +8,7 @@ sleep 1
 curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car
 ls -lh partial.car
 /usr/local/bin/car ls -v partial.car
+rm /var/log/nginx/error.log
 
 curl -o partial.car -s -w "time: %{time_total} s\n" -H "Accept: application/vnd.ipld.car" -m 5  http://127.0.0.1:8080/fixture.car?bytes=0:1048576
 ls -lh partial.car
