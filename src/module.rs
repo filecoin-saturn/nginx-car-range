d20f431ce4ac90d37cc15b5091c338ef0ce03c5e
tchardin <tdotchardin@gmail.com>
2023-04-15 14:02:03 +0200

    log buf content

--- src/module.rs
+++ src/module.rs
@@ -150,7 +150,19 @@ fn log_buf_info(r: &mut Request, chain: *mut ngx_chain_t, tag: &str) {
         let buf = unsafe { MemoryBuffer::from_ngx_buf((*cl).buf) };
         cl = unsafe { (*cl).next };
 
-        ngx_log_debug_http!(r, "car_range {} buf chain: size {}", tag, buf.len());
+        let bytes = if buf.len() < 256 {
+            hex::encode(buf.as_bytes())
+        } else {
+            String::new()
+        };
+
+        ngx_log_debug_http!(
+            r,
+            "car_range {} buf chain: size {}, bytes {}",
+            tag,
+            buf.len(),
+            bytes
+        );
     }
 
     if chain.is_null() {
