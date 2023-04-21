cee5a37d02712c941fc54b423314624a654ea16d
tchardin <tdotchardin@gmail.com>
2023-04-21 10:27:58 +0200

    print logs and exit

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ set -x #echo on
 test_range_request () {
   range="$1"
   code="$(curl -sw "%{http_code}\n" -o partial.car -H "Accept: application/vnd.ipld.car" "http://127.0.0.1:8080/midfixture.car?bytes=${range}")"
-  test "$code" -eq 200 || exit 1
+  test "$code" -eq 200 || cat /var/log/nginx/error.log && exit 1
   ls -lh partial.car
   /usr/local/bin/car ls -v partial.car
 }
@@ -21,5 +21,3 @@ test_range_request "0:1048576"
 
 test_range_request "1048576:2097152"
 
-sleep 1
-cat /var/log/nginx/error.log
