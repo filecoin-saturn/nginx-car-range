4b38bfe04fe9a38c4506420e982e23340db4eaad
tchardin <tdotchardin@gmail.com>
2023-06-19 16:20:45 -0600

    initial test case for a range offset

--- src/car_reader.rs
+++ src/car_reader.rs
@@ -106,48 +106,50 @@ impl<'a, R: RangeBounds<u64> + Clone, A: Allocator> CarBufferContext<'a, R, A> {
             cl = unsafe { (*cl).next };
 
             // TODO: handle internal errors
-            let (start, end) = self.framed.next(buf.as_bytes()).unwrap();
+            let parts = self.framed.next(buf.as_bytes()).unwrap();
 
-            self.pos = end;
-            let sub = buf.len() - end;
+            for (start, end) in parts {
+                self.pos = end;
+                let sub = buf.len() - end;
 
-            let is_last = match self.framed.range.end_bound() {
-                Bound::Included(&b) => b == self.framed.unixfs_read as u64,
-                Bound::Excluded(&b) => b - 1 == self.framed.unixfs_read as u64,
-                // if the range is unbounded the last buffer should already be
-                // set as last.
-                Bound::Unbounded => false,
-            };
+                let is_last = match self.framed.range.end_bound() {
+                    Bound::Included(&b) => b == self.framed.unixfs_read as u64,
+                    Bound::Excluded(&b) => b - 1 == self.framed.unixfs_read as u64,
+                    // if the range is unbounded the last buffer should already be
+                    // set as last.
+                    Bound::Unbounded => false,
+                };
 
-            if sub > 0 || is_last {
-                self.done = 1;
-                buf.set_last_buf(true);
-                buf.set_last_in_chain(true);
-            }
-
-            if sub == buf.len() {
-                buf.set_empty();
-                continue;
-            }
-
-            let mut cl = self.pool.alloc_chain();
-            if cl.is_null() {
-                continue;
-            }
-            unsafe {
-                (*cl).buf = buf.as_ngx_buf_mut();
-                (*cl).next = std::ptr::null_mut();
-
-                if sub > 0 {
-                    ngx_buf_remove_end((*cl).buf, sub);
+                if sub > 0 || is_last {
+                    self.done = 1;
+                    buf.set_last_buf(true);
+                    buf.set_last_in_chain(true);
                 }
 
-                if start > 0 {
-                    ngx_buf_remove_start((*cl).buf, start);
+                if sub == buf.len() {
+                    buf.set_empty();
+                    continue;
                 }
+
+                let mut cl = self.pool.alloc_chain();
+                if cl.is_null() {
+                    continue;
+                }
+                unsafe {
+                    (*cl).buf = buf.as_ngx_buf_mut();
+                    (*cl).next = std::ptr::null_mut();
+
+                    if sub > 0 {
+                        ngx_buf_remove_end((*cl).buf, sub);
+                    }
+
+                    if start > 0 {
+                        ngx_buf_remove_start((*cl).buf, start);
+                    }
+                }
+                *ll = cl;
+                ll = unsafe { &mut (*cl).next };
             }
-            *ll = cl;
-            ll = unsafe { &mut (*cl).next };
         }
 
         out
@@ -248,6 +250,8 @@ struct Framed<R: RangeBounds<u64> + Clone> {
     unixfs_read: usize,
     // the size of the unixfs frame
     unixfs_len: usize,
+    // if the current dag node has links in which case it will be included
+    has_links: bool,
     // the current frame type
     state: FrameType,
 }
@@ -260,6 +264,7 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
             blk_pos: 0,
             unixfs_read: 0,
             unixfs_len: 0,
+            has_links: false,
             range,
             buf: Vec::with_capacity(72),
             state: FrameType::CarHeader,
@@ -268,18 +273,29 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
 
     // reads all the frames in the buffer returning the number of bytes to remove from the start
     // and end.
-    fn next(&mut self, buf: &[u8]) -> io::Result<(usize, usize)> {
+    fn next(&mut self, buf: &[u8]) -> io::Result<Vec<(usize, usize)>> {
+        let mut ranges = Vec::with_capacity(2);
+        let mut start = 0;
         let mut pos = 0;
+        let mut maybe = 0;
         let mut current = buf;
         while current.len() > 0 {
+            if gt_bound(self.range.end_bound(), self.unixfs_read as u64) {
+                ranges.push((start, pos));
+                return Ok(ranges);
+            }
             if self.state == FrameType::Cid {
                 match self.decode_cid(current) {
                     Some((cid, read)) => {
+                        println!("cid: {:?}, read {}", cid, read);
                         self.state = FrameType::Block;
                         current = &current[read..];
 
                         if self.include_block() {
                             pos += read;
+                        } else {
+                            println!("skipping block pos: {}", pos);
+                            maybe += read;
                         }
 
                         match cid.codec() {
@@ -300,7 +316,9 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                     None => {
                         current = &[];
 
-                        if self.include_block() {
+                        // bit of a hack but we assume that a unixfs chunk won't be smaller
+                        // than 1kb so we consider it some kind of intermediate node and include it.
+                        if self.include_block() || self.blk_len < 1000 {
                             pos = buf.len();
                         }
 
@@ -312,11 +330,15 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
             if self.len == 0 {
                 match self.decode_len(current) {
                     Some((size, read)) => {
+                        println!("decoded size: {}, read: {}", size, read);
                         current = &current[read..];
                         self.len = size;
 
                         if self.include_block() {
                             pos += read;
+                        } else {
+                            println!("skipping block pos: {}", pos);
+                            maybe += read;
                         }
 
                         match self.state {
@@ -324,6 +346,7 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                                 self.state = FrameType::Cid;
                                 self.blk_len = size;
                                 self.len = 0;
+                                self.has_links = false;
                             }
                             FrameType::MerkleDag => {
                                 self.blk_pos += read;
@@ -379,6 +402,8 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                             }
                             FrameType::PBLinks => {
                                 self.blk_pos += read;
+                                self.has_links = true;
+                                println!("blk len: {}, blk pos: {}", self.blk_len, self.blk_pos);
                             }
                             FrameType::UnixFsData => {
                                 self.blk_pos += read;
@@ -392,9 +417,27 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                                 self.blk_pos += read;
                                 self.len = 0;
 
+                                println!("left {}", current.len());
+
+                                if matches!(self.state, FrameType::DataType) {
+                                    let tp = size as i32;
+                                    let dt: DataType = tp.try_into().unwrap();
+                                    println!("data type: {:?}", dt);
+                                }
+
                                 if self.blk_len - self.blk_pos == 0 {
                                     self.state = FrameType::Block;
                                     self.blk_pos = 0;
+                                    println!(
+                                        "end of block, maybe: {}, pos {}, unixfs_len {}",
+                                        maybe, pos, self.unixfs_len
+                                    );
+                                    // include any intermediary blocks so they are ones
+                                    // with no unixfs data
+                                    if self.unixfs_len == 0 {
+                                        pos += maybe;
+                                        maybe = 0;
+                                    }
                                 } else {
                                     self.state = FrameType::UnixFs;
                                 }
@@ -406,6 +449,9 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                         current = &[];
                         if self.include_block() {
                             pos = buf.len();
+                        } else {
+                            println!("skipping block pos: {}", pos);
+                            maybe = buf.len();
                         }
                         if matches!(
                             self.state,
@@ -423,10 +469,19 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                     }
                 };
 
+                if self.has_links {
+                    pos += maybe;
+                    maybe = 0;
+                }
+
             // end of the frame
             } else if current.len() >= self.len {
+                println!("end of frame, len: {}", self.len);
                 if self.include_block() {
                     pos += self.len;
+                } else {
+                    println!("skipping block pos: {}", pos);
+                    maybe += self.len;
                 }
                 match self.state {
                     FrameType::CarHeader => {
@@ -437,24 +492,49 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                         self.blk_pos += self.len;
                     }
                     FrameType::UnixFsData | FrameType::RawLeaf => {
+                        if maybe > 0 {
+                            println!(
+                                "pushing range start {}, pos {}, maybe {}",
+                                start, pos, maybe
+                            );
+                            if pos > start {
+                                ranges.push((start, pos));
+                            }
+                            start = start + pos + maybe;
+                            pos = start;
+                            maybe = 0;
+                        }
+
                         self.blk_pos = 0;
                         self.state = FrameType::Block;
                         self.unixfs_read += self.unixfs_len;
                         self.unixfs_len = 0;
+
+                        println!(
+                            "end of unixfs chunk: pos: {}, maybe: {}, start: {}",
+                            pos, maybe, start
+                        );
                     }
                     _ => {}
                 };
                 current = &current[self.len..];
                 self.len = 0;
-
             // partial frame
             } else {
+                println!("partial frame, len: {}", current.len());
                 if self.include_block() {
                     pos += current.len();
+                } else {
+                    println!("skipping block pos: {}", pos);
+                    maybe += current.len();
                 }
+
                 match self.state {
                     FrameType::PBLinks => {
                         self.blk_pos += current.len();
+
+                        // Assume if we have pblink frame we should include this intermediary node
+                        pos += maybe;
                     }
                     FrameType::UnixFsData => {
                         self.blk_pos += current.len();
@@ -465,15 +545,20 @@ impl<R: RangeBounds<u64> + Clone> Framed<R> {
                 current = &[];
             }
         }
-        Ok((0, pos))
+        ranges.push((start, pos));
+        Ok(ranges)
     }
 
     // since the end bound is inclusive, we add 1 to the unixfs cursor
     fn include_block(&self) -> bool {
+        println!(
+            "include block: {:?}, unixfs_read {}, unixfs_len {}",
+            self.state, self.unixfs_read, self.unixfs_len
+        );
         match self.state {
             FrameType::CarHeader => true,
             FrameType::UnixFsData | FrameType::RawLeaf => {
-                if self.unixfs_read == 0 {
+                if self.unixfs_read == 0 || self.unixfs_len == 0 {
                     self.range.contains(&(self.unixfs_read as u64 + 1))
                 } else {
                     ranges_overlap(
@@ -1366,6 +1451,41 @@ mod tests {
         assert_eq!(buf, exp);
     }
 
+    // check the CAR file is a valid car file and contains the given blocks only
+    fn check_car(buf: &[u8], blks: Vec<Cid>) {
+        let mut current = buf;
+        let (size, read) = usize::decode_var(current).unwrap();
+        let header: CarHeader =
+            serde_ipld_dagcbor::from_slice(&current[read..size + read]).unwrap();
+        assert_eq!(header.roots[0], blks[0]);
+        current = &buf[size + read..];
+
+        for cid in blks {
+            let (size, read) = usize::decode_var(current).unwrap();
+            let mut reader = Cursor::new(&current[read..]);
+            assert_eq!(cid, Cid::read_bytes(&mut reader).unwrap());
+
+            current = &current[size + read..];
+        }
+        assert_eq!(current.len(), 0);
+    }
+
+    struct TC {
+        range: Range<u64>,
+        size: usize,
+        blks: Vec<Cid>,
+    }
+
+    impl TC {
+        fn new(range: Range<u64>, size: usize, blks: Vec<&str>) -> Self {
+            Self {
+                range,
+                size,
+                blks: blks.iter().map(|b| b.clone().try_into().unwrap()).collect(),
+            }
+        }
+    }
+
     #[test]
     fn test_frame_loop() {
         use std::fs::File;
@@ -1377,7 +1497,46 @@ mod tests {
         let mut car_data = vec![];
         reader.read_to_end(&mut car_data).unwrap();
 
-        let ranges = [(0..7000, 6805), (0..1500, 2538), (0..1025, 1465)];
+        let ranges = [
+            TC::new(
+                0..7000,
+                6805,
+                vec![
+                    "bafybeidutwlr3tfcjuytejeusetv65lltrw6epessuyjzxhjg3kk4wt6ea",
+                    "bafybeihrwler3wpt3vws4eutcr6cyqhielsin2hne6ogzkfarfoitt3jwa",
+                    "bafybeigy57ubfegrckt4dq33s2g2vcwiahct5fgki6s2likb2yvztft63y",
+                    "bafybeia42q3rzcpjozdz7o6n6l6w6nuaugm3x7y2are4b6nfid2yhnw3oa",
+                    "bafybeiew7intdgz5ghmxoygcx77jdd3exsaw22g4nlr42oi27dytbbbfcm",
+                    "bafybeicp6ecmucwm2remreb5ah7j26yuihr3l42lcmuff7fguuqwj4jgoq",
+                    "bafybeicbb6atqrrq3wyrde56qteh6gk35gmjdq2n62iafs6qi3xltjpxoq",
+                ],
+            ),
+            TC::new(
+                0..1500,
+                2538,
+                vec![
+                    "bafybeidutwlr3tfcjuytejeusetv65lltrw6epessuyjzxhjg3kk4wt6ea",
+                    "bafybeihrwler3wpt3vws4eutcr6cyqhielsin2hne6ogzkfarfoitt3jwa",
+                    "bafybeigy57ubfegrckt4dq33s2g2vcwiahct5fgki6s2likb2yvztft63y",
+                ],
+            ),
+            TC::new(
+                0..1025,
+                1465,
+                vec![
+                    "bafybeidutwlr3tfcjuytejeusetv65lltrw6epessuyjzxhjg3kk4wt6ea",
+                    "bafybeihrwler3wpt3vws4eutcr6cyqhielsin2hne6ogzkfarfoitt3jwa",
+                ],
+            ),
+            TC::new(
+                1025..1048,
+                1465,
+                vec![
+                    "bafybeidutwlr3tfcjuytejeusetv65lltrw6epessuyjzxhjg3kk4wt6ea",
+                    "bafybeigy57ubfegrckt4dq33s2g2vcwiahct5fgki6s2likb2yvztft63y",
+                ],
+            ),
+        ];
 
         for range in ranges.iter() {
             let factors = [1, 5, 12, 31, 40, 55, 120, 300];
@@ -1387,27 +1546,25 @@ mod tests {
 
                 let sections = car_data.chunks(section_size);
 
-                let mut reader = Framed::new(range.0.clone());
+                let mut reader = Framed::new(range.range.clone());
 
                 let mut buf = vec![];
 
                 for section in sections {
                     println!("new section of size {}", section.len());
                     match reader.next(section) {
-                        Ok((start, end)) => {
-                            println!("start {} end {}", start, end);
-                            buf.extend_from_slice(&section[start..end]);
+                        Ok(parts) => {
+                            for (start, end) in parts {
+                                println!("=> start {} end {}", start, end);
+                                buf.extend_from_slice(&section[start..end]);
+                            }
                         }
                         Err(e) => panic!("failed to read all bytes for factor {}: {}", factor, e),
                     }
                 }
-                assert_eq!(buf.len(), range.1);
+                assert_eq!(buf.len(), range.size);
 
-                assert_eq!(
-                    reader.unixfs_read, 6119,
-                    "failed to read all bytes for factor {}",
-                    factor
-                );
+                check_car(&buf[..], range.blks.clone());
 
                 println!("\n");
             }
